6277396eeb399d910d1d7a4a5cee7895188aa226
NikolayBalabanov <96470401+NikolayBalabanov@users.noreply.github.com>
2022-09-13 20:21:44 +0300

    fix: slider in my-projects section; docs: added focus-visible js lib; feat: hover, focus, active effects added

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,56 @@
-const swiper = new Swiper('.swiper', {
-  // Default parameters
-  speed: 700,
-  slidesPerView: 2,
-  spaceBetween: 15,
-  pagination: {
-    el: ".swiper .swiper__pagination",
-    type: "fraction"
+const partnersSlider = new Swiper(".swiper", {
+  slidesPerView: 1,
+  grid: {
+    rows: 1,
+    fill: "row"
   },
+  spaceBetween: 0,
   navigation: {
     nextEl: ".swiper__btn_next",
     prevEl: ".swiper__btn_prev"
   },
+  speed: 700,
+  breakpoints: {
+
+    760: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+      slidesPerGroup: 2,
+    }
+  },
+
   a11y: false,
-  keyboard: true, // можно управлять с клавиатуры стрелками влево/вправо
+  keyboard: true,
+  a11y: false,
   watchSlidesProgress: true,
   watchSlidesVisibility: true,
   slideVisibleClass: 'slide-visible',
-  parallax: true,
-  autoplay: {
-    delay: 8000,
-  },
-  // breakpoints: {
-  //   // when window width is >= 320px
-  //   320: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20,
-  //     slidesPerGroup: 2,
-  //   },
-  //   // when window width is >= 480px
-  //   480: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 30,
-  //     slidesPerGroup: 2,
-  //   },
-  //   // when window width is >= 640px
-  //   640: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 15,
-  //     slidesPerGroup: 2,
-  //   }
-  // }
-})
+
+  on: {
+
+    init: function () {
+      this.slides.forEach(slide => {
+        if (!slide.classList.contains('slide-visible')) {
+          slide.querySelector('.swiper-slide__link').tabIndex = '-1';
+        } else {
+          slide.querySelector('.swiper-slide__link').tabIndex = '';
+        }
+      });
+    },
+
+    slideChange: function () {
+      this.slides.forEach(slide => {
+        if (!slide.classList.contains('slide-visible')) {
+          slide.querySelector('.swiper-slide__link').tabIndex = '-1';
+        } else {
+          slide.querySelector('.swiper-slide__link').tabIndex = '';
+        }
+      });
+    }
+  }
+});
