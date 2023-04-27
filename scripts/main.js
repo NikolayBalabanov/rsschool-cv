b5743074a304663e2e4ded0d84afe84bb255c076
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-04-27 21:27:06 +0300

    feat: update projects section

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,29 @@
-const partnersSlider = new Swiper('.swiper', {
-  slidesPerView: 1,
-  grid: {
-    rows: 1,
-    fill: 'row'
-  },
-  spaceBetween: 0,
-  navigation: {
-    nextEl: '.swiper__btn_next',
-    prevEl: '.swiper__btn_prev'
-  },
-  speed: 700,
-  breakpoints: {
-
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 50,
-    },
-
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 25,
-      slidesPerGroup: 2,
-    }
-  },
-
-  a11y: false,
-  keyboard: true,
-  a11y: false,
-  watchSlidesProgress: true,
-  watchSlidesVisibility: true,
-  slideVisibleClass: 'slide-visible',
-
-  on: {
-
-    init: function () {
-      this.slides.forEach(slide => {
-        if (!slide.classList.contains('slide-visible')) {
-          slide.querySelector('.swiper-slide__link').tabIndex = '-1';
-        } else {
-          slide.querySelector('.swiper-slide__link').tabIndex = '';
-        }
-      });
-    },
-
-    slideChange: function () {
-      this.slides.forEach(slide => {
-        if (!slide.classList.contains('slide-visible')) {
-          slide.querySelector('.swiper-slide__link').tabIndex = '-1';
-        } else {
-          slide.querySelector('.swiper-slide__link').tabIndex = '';
-        }
-      });
-    }
-  }
-});
-
-
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.header__nav');
 const navLinks = document.querySelectorAll('.header__nav .nav__item .nav__link');
 
-
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger--active');
   menu.classList.toggle('nav--active');
-})
+});
 
-navLinks.forEach(el => {
+navLinks.forEach((el) => {
   el.addEventListener('click', () => {
     burger.classList.remove('burger--active');
     menu.classList.remove('nav--active');
-  })
-})
+  });
+});
 
+// projects
+const projectDetailsBtns = document.querySelectorAll('.project__show-btn');
+const projectsContainer = document.querySelector('.projects__content');
+projectsContainer.addEventListener('click', (event) => {
+  if (event.target.classList.contains('project__show-btn')) {
+    if (event.target.parentNode.classList.contains('is-active')) {
+      event.target.parentNode.classList.toggle('is-active');
+    } else {
+      projectDetailsBtns.forEach((el) => el.parentNode.classList.remove('is-active'));
+      event.target.parentNode.classList.toggle('is-active');
+    }
+  }
+});
